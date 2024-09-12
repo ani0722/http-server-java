4acfe016d8c9623105a9f1f6b8c42e8893302a19
Anish Kothari <anish0722@gmail.com>
2024-09-12 13:29:55 +0530

    [pass 4th stage]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,30 +7,33 @@ import java.net.ServerSocket;
 import java.net.Socket;
 public class Main {
   public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    // You can use print statements as follows for debugging, they'll be visible
+    // when running tests.
     System.out.println("Logs from your program will appear here!");
     ServerSocket serverSocket = null;
     Socket clientSocket = null;
     try {
       serverSocket = new ServerSocket(4221);
-    
       serverSocket.setReuseAddress(true);
-    
       clientSocket = serverSocket.accept(); // Wait for connection from client.
+      System.out.println("accepted new connection");
       InputStream input = clientSocket.getInputStream();
       BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-      String line = reader.readLine(); // Read the first line from client.
-      System.out.println(line);
-      
+      String line = reader.readLine();
       String[] HttpRequest = line.split(" ", 0);
-      OutputStream output = clientSocket.getOutputStream();
-
       if (HttpRequest[1].equals("/")) {
-        output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+        clientSocket.getOutputStream().write(
+            "HTTP/1.1 200 OK\r\n\r\n".getBytes());
+      } else if (HttpRequest[1].startsWith("/echo/")) {
+        String msg = HttpRequest[1].substring(6);
+        String header = String.format(
+            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
+            msg.length(), msg);
+        clientSocket.getOutputStream().write(header.getBytes());
       } else {
-        output.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+        clientSocket.getOutputStream().write(
+            "HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
       }
-      System.out.println("accepted new connection");
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     }
